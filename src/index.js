a379c793e4725a69218b7987bae23e77cc4db63d
Fred Nordell <dat15fno@student.lu.se>
2018-06-10 20:57:20 +0200

    added react boilerplate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker.js';
+
+const root = document.getElementById('root')
+if (root) {
+ReactDOM.render(<App />, root);
+registerServiceWorker();
+} else {
+	console.error("Couldn't find root node, exiting")
+}
