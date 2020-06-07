c8fa142b3ef3b6ff0005b1aae6b627ec6ca464d1
Fred Nordell <dat15fno@student.lu.se>
2020-06-07 15:43:16 +0200

    added a pic to gallery, updated copyright layuot

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Link from "./Link";
+import { Grid } from "@material-ui/core";
 
 export default function Copyright() {
   return (
-    <Typography
-      id="copyright"
-      variant="body2"
-      color="textSecondary"
-      align="center"
+    <Grid
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="flex-end"
     >
-      This work is licensed under the <br />
-      <a
-        rel="noreferrer noopener license"
-        target="_blank"
-        href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+      <Typography
+        id="copyright"
+        variant="body2"
+        color="textSecondary"
+        align="left"
       >
-        Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
-        International License
-      </a>
-      <br />
+        This work is licensed under the{" "}
+        <a
+          rel="noreferrer noopener license"
+          target="_blank"
+          href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+        >
+          Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+          International License
+        </a>
+      </Typography>
       <a
         rel="noreferrer noopener license"
         target="_blank"
@@ -30,6 +37,6 @@ export default function Copyright() {
           src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
         />
       </a>
-    </Typography>
+    </Grid>
   );
 }
